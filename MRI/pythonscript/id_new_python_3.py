6946a411528242da5032bac2fcff9386b330c33b
ryoiwata <ryoiwata@gmail.com>
2021-09-16 15:27:04 -0400

    updated python scripts to python 3

---
+++ MRI/pythonscript/id_new_python_3.py
@@ -0,0 +1,105 @@
+'''This script is used to generate the input for scanner version of ALL ED movie.
+'''
+from time import *
+from random import *
+import math
+#Possible combination of ED and ID
+MainList = ["L","R","T", "C", "B","W"]
+lr = ["L", "R"]
+tc = ["T", "C"]
+bw = ["B", "W"]
+ed_num = 22
+id_num = 10
+global groupStr
+#outFile = open("out_new.txt", 'a')
+def openOutFile():
+	'''Open a file to write output
+	'''
+	try:
+		outFile = open("ed_out_new.txt", 'a');
+		return outFile
+	except:
+		print("output file coule not be opened!!")
+
+def checkTwoNearest(tempList):
+	''' Two nearest elements in the list should be different
+	'''
+	rtnVal = True
+	for i in range(len(tempList)-1):
+		if (tempList[i] == tempList[i+1]):
+			rtnVal = False
+			break
+	return rtnVal
+
+def testExactMovingAverage(inList, group):
+	'''Test if the moving average is exact for the group
+	'''
+	rtnVal = True
+	pct50 = group/2
+	pct25 = group/4
+	for i in range(len(inList)-group):
+		myList = inList[i:(group+i)]
+		if( (myList.count(4) - pct50) == 0 ):
+			if( (myList.count(5) - pct25) == 0 ):
+				if( (myList.count(6) - pct25) == 0 ):
+				 	pass
+		else:
+			rtnVal = False
+			break
+	if(rtnVal == True):
+		print("PASSED - moving avg. ", group, " : " , inList)
+	else:
+		print(i)
+	return rtnVal
+def checkApproxMovingAverage(inList, group):
+	'''Check if every 12 contiguous elemets have 50% of 4, and 25% of 5 and 6
+	'''
+	#print "moving avg. ", group
+	global groupStr
+	rtnVal = True
+	pct50 = group/2
+	pct25 = group/4
+	for i in range(len(inList)-group):
+		myList = inList[i:(group+i)]
+		if( (myList.count(4) - pct50) in [-1, 0, 1]): # One more or less is accepted in the group
+			if( (myList.count(5) - pct25) in [-1, 0, 1] ):
+				if( (myList.count(6) - pct25) in [-1, 0, 1] ):
+				 	pass
+		else:
+			rtnVal = False
+			break
+	if(rtnVal == True):
+		print("Passed moving avg. ", group)
+		groupStr = "Passed moving avg. " + str(group)
+		#oFile.write("\n" + "Passed moving avg. " + repr(group) +"\n")
+	return rtnVal
+def makeCriteriaList():
+	ed = [4] * 17 + [5] * 8 + [6] * 8
+	done = 0
+	tmpED = ed[:]
+	while(done==0):
+		shuffle(tmpED)
+		if ( checkApproxMovingAverage(tmpED, 8) ):
+		#or checkApproxMovingAverage(tmpED, 16) or checkApproxMovingAverage(tmpED, 20) ):
+			done = 1
+		else:
+			tmpED = ed[:]
+	print(tmpED)
+	return tmpED
+
+	return outList
+def main(count=1):
+	oFile = openOutFile()
+	oFile.write("\n" + ctime() + "\n")
+	try:
+		global groupStr
+		for i in range(count):
+			'''
+			stmList = makeInputStim()
+			criteriaList = makeCriteriaList()
+			'''
+			criteriaList = makeCriteriaList()
+			oFile.write(groupStr + "\n")
+			oFile.write("\n" + repr(criteriaList)+"\n")
+	finally:
+		oFile.close()
---
+++ results/README.md
@@ -0,0 +1,6 @@
+# Reversal Learning Project Log
+
+## 2021, September 16th
+- Updated Python scripts from Python 2 to 3
+    - MRI/pythonscript/id_new_python_3.py
+    - MRI/pythonscript ID4/id4_python_3.py
